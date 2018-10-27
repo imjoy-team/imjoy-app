f4da190f9d5a66be3cdb422e675e501a1b259c5d
oeway <oeway007@gmail.com>
2018-10-27 18:07:21 +0200

    add electron-packager

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow()
+  mainWindow = new BrowserWindow({icon: __dirname + '/utils/imjoy.ico'})
 
   // and load the index.html of the app.
   // mainWindow.loadFile('index.html')
